8d5cae68439fba7d61a34e72d7937bf2e841643c
markopedu <69790644+markopedu@users.noreply.github.com>
2021-01-07 08:27:47 +0100

    Initial Commit

---
+++ QuickCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuickCard
+//
+//  Created by Marko Poikkimäki on 2021-01-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuickCard/QuickCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickCardApp.swift
+//  QuickCard
+//
+//  Created by Marko Poikkimäki on 2021-01-07.
+//
+
+import SwiftUI
+
+@main
+struct QuickCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
